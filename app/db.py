7a3c6bf1c698f73657e5250780219f799fb8d687
Daniel Joseph <danielomololu2002@gmail.com>
2025-10-12 20:17:18 +0100

    Updated test and command function

--- app/db.py
+++ app/db.py
@@ -12,6 +12,7 @@ def get_db():
                  detect_types=sqlite3.PARSE_DECLTYPES
                 )
     g.db.row_factory = sqlite3.Row
+    g.db.execute('PRAGMA foreign_keys = ON')
     return g.db
 
 @app.teardown_appcontext
@@ -30,35 +31,35 @@ def init_db():
 
 @click.command('seed-db')
 def seed_db():
-    from app.models import Role
     from app.services import FacilityServices, UserServices, DiseaseServices, DiseaseCategoryServices
     import random
-    f = FacilityServices.create_facility('SSH Okitipupa', 'okitipupa', 'tertiary')
-    click.echo(f"Added facility id: {f.id}, name: {f.name}, local_government = {f.local_government}, type: {f.facility_type}")
-    u = UserServices.create_user('betatester', 1, 'betatesterpassword')
-    click.echo(f"Created user username: {u.username}, facility_id: {u.facility_id}, role: {u.role.name}")
-    u = UserServices.create_user('betaadmintester', None, 'betaadmintesterpassword', role = Role.admin)
-    click.echo(f"Created user username: {u.username}, facility_id: {u.facility_id}, role: {u.role.name}")
     category_list = ['SURGICAL', 'OBSTERIC', 'NON-COMMUNICABLE DISEASE', 'COMMUNICABLE DISEASE', 'PSYCHIATRY', 
-                     'GYNAECOLOGICAL', 'PEADIATRICS']
+                        'GYNAECOLOGICAL', 'PEADIATRICS']
     disease_list = ["ABSCESS","ANEMIA","ANEMIA PREGNANCY","ANXIETY","A. P. H.","APPENDICITIS","ARTRITIS /RHEUMATISM","ASSITED DEV.","ASTHMA","BITE (HUMAN)",
-"BITE (SCORPION)","BITE (SNAKE","BITE(DOG)","BOIL","BREACH DEL","BRONCHITIS","C. C.  F.","C.V.D.","C/S","CELLULITIS.","CERE.VAS ACC",
-"CEREBRAL PALSY","CHICKEN POX","CHOLECYSTITIS","CHOLERA","CONJUCTIVITIS","DERMATITIS & OTHER SKIN DIS. ","DIAB. MELLIT (DM)","DIARRHOEA",
-"DISLOCATION","DRUG REACTION ","DYSENTARY","DYSPLEGIA","ECLAMSIA","ECTOPIC PREG.","ENDOCROUTE/ NUTRITIONAL DIS. ","EPILESY ","FIBROID",
-"FILARIASIS","FRACTURE","GASTRO ENT.","GINGIVITIS","GUN SHOT","HAEMATOMA","HAEMORRHAGE","HEMORRHOID","HEPATITIS","HERNIA","HIV/AID",
-"HYDROCEPHALUS","HYPERTENSION (HTN)","HYPOGLYCAEMIA","HYPOTENSION","INJURY ","INTESTNAL OBS","JAUNDICE","KERATITIS","LIPOMA","CYESIS",
-"MALARIA ","MALARIA IN PREGNANCY","MASTITIS","MEASLES","MENINGITIS"," MENTAL DISORDERS ","MIGRAINE","MUMPS","NEPHRITIS & OTHER KIDNEY  DIS.",
-"NORMAL DEL.","OSTEOMYELITIS","OTITIS MED","OVARIAN  CYST","P . P. H ","PERITONITIS","PID","PLACENTAL PREVIA","PNEUMOMIA","POISON",
-"PREG. INDUCE HYPERTENSION","R.T.A & OTHER ACC","REP. TRAC. INFEC. (RTI)","RUPTURED UTERINE","SEPSIS","SICKLE CELL ANAEMIA","STD",
-"STILL BIRTH","STOMATITIS","TETANUS","THREATEN ABORTION","TINEA CAPITI./CORPORIS","TONGUE TIE","TONSILITIS","TRAUMA","TUBERCULOSIS",
-"TYPHOID FEVER","ULCER","HYSTECTOCMY","UTI","VESICO-VAGINAL-FISTULA"] 
+    "BITE (SCORPION)","BITE (SNAKE","BITE(DOG)","BOIL","BREACH DEL","BRONCHITIS","C. C.  F.","C.V.D.","C/S","CELLULITIS.","CERE.VAS ACC",
+    "CEREBRAL PALSY","CHICKEN POX","CHOLECYSTITIS","CHOLERA","CONJUCTIVITIS","DERMATITIS & OTHER SKIN DIS. ","DIAB. MELLIT (DM)","DIARRHOEA",
+    "DISLOCATION","DRUG REACTION ","DYSENTARY","DYSPLEGIA","ECLAMSIA","ECTOPIC PREG.","ENDOCROUTE/ NUTRITIONAL DIS. ","EPILESY ","FIBROID",
+    "FILARIASIS","FRACTURE","GASTRO ENT.","GINGIVITIS","GUN SHOT","HAEMATOMA","HAEMORRHAGE","HEMORRHOID","HEPATITIS","HERNIA","HIV/AID",
+    "HYDROCEPHALUS","HYPERTENSION (HTN)","HYPOGLYCAEMIA","HYPOTENSION","INJURY ","INTESTNAL OBS","JAUNDICE","KERATITIS","LIPOMA","CYESIS",
+    "MALARIA ","MALARIA IN PREGNANCY","MASTITIS","MEASLES","MENINGITIS"," MENTAL DISORDERS ","MIGRAINE","MUMPS","NEPHRITIS & OTHER KIDNEY  DIS.",
+    "NORMAL DEL.","OSTEOMYELITIS","OTITIS MED","OVARIAN  CYST","P . P. H ","PERITONITIS","PID","PLACENTAL PREVIA","PNEUMOMIA","POISON",
+    "PREG. INDUCE HYPERTENSION","R.T.A & OTHER ACC","REP. TRAC. INFEC. (RTI)","RUPTURED UTERINE","SEPSIS","SICKLE CELL ANAEMIA","STD",
+    "STILL BIRTH","STOMATITIS","TETANUS","THREATEN ABORTION","TINEA CAPITI./CORPORIS","TONGUE TIE","TONSILITIS","TRAUMA","TUBERCULOSIS",
+    "TYPHOID FEVER","ULCER","HYSTECTOCMY","UTI","VESICO-VAGINAL-FISTULA"] 
+    db =  get_db()
     for x in category_list:
         DiseaseCategoryServices.create_category(x, commit=False)
     click.echo(f"Added {len(category_list)} disease categories to the database")
     for x in disease_list:
+        #random disease cateogry for testing
         DiseaseServices.create_disease(x, random.randint(1, len(category_list)), commit = False)
     click.echo(f"Added {len(disease_list)} diseases to the database")
-    db =  get_db()
+    db.commit()
+
+    import app.seed
+    from app.models import Role
+    u = UserServices.create_user('betaadmintester', None, 'betaadmintesterpassword', role = Role.admin)
+    click.echo(f"Created user username: {u.username}, facility_id: {u.facility_id}, role: {u.role.name}")
     db.commit()
 
     click.echo("Seeded the database")
