33646c25fd4230ca37b964c81ab0ddf7f3eeaaed
Daniel Joseph <danielomololu2002@gmail.com>
2025-10-24 15:39:28 +0100

    Fix scheme filtering bug

--- app/config.py
+++ app/config.py
@@ -25,7 +25,11 @@ LOCAL_GOVERNMENT = set([
     "Ose".lower(),
     "Owo".lower()])
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DBNAME = 'odchc_encounter.db'
+DBPATH = os.path.join(os.path.dirname(BASE_DIR), DBNAME)
+# print(DBPATH)
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or "This is supposed to be a secret key"
     ADMIN_PAGE_PAGINATION = 15
-    DATABASE = os.getenv('ODCHC_DATABASE') or 'odchc_encounter.db'
+    DATABASE = os.getenv('ODCHC_DATABASE') or DBPATH
