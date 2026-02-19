f1665b57f3ab676d4db8681fd6210e7a73ec66fd
Daniel Joseph <danielomololu2002@gmail.com>
2026-02-19 14:15:52 +0100

    Fix llm schema path error

--- app/config.py
+++ app/config.py
@@ -12,6 +12,6 @@ class Config:
         'SECRET_KEY') or "This is supposed to be a secret key"
     ADMIN_PAGE_PAGINATION = 15
     DATABASE = os.getenv('ODCHC_DATABASE') or DBPATH
-    LLM_SCHEMA_PATH = os.getenv('LLM_SCHEMA_PATH')
+    LLM_SCHEMA_PATH = os.path.join(BASE_DIR, 'llm_minimal_schema.txt')
     GOOGLE_GENAI_API_KEY = os.getenv('GOOGLE_GENAI_API_KEY')
     GOOGLE_GENAI_MODEL = 'gemini-2.5-flash-lite'
